f83c7f6cbd613e11056e3fc407b1b486fa08a346
kyungtae42 <kimbam1017@naver.com>
2024-07-22 16:54:51 +0900

    🩹 fix : 주석제거

--- src/main/java/com/sparta/publicclassdev/domain/user/entity/RoleEnum.java
+++ src/main/java/com/sparta/publicclassdev/domain/user/entity/RoleEnum.java
@@ -1,9 +1,9 @@
 package com.sparta.publicclassdev.domain.user.entity;
 
 public enum RoleEnum {
-    USER(Authority.USER),  // 사용자 권한
-    ADMIN(Authority.ADMIN),  // 관리자 권한
-    WITHDRAW(Authority.WITHDRAW); // 휴면 계정
+    USER(Authority.USER),
+    ADMIN(Authority.ADMIN),
+    WITHDRAW(Authority.WITHDRAW);
     private final String authority;
 
     RoleEnum(String authority) {
