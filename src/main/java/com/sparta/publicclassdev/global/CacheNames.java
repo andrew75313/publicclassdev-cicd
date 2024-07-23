822619883de133eac93a1f5c0821cb516d2feaea
kyungtae42 <kimbam1017@naver.com>
2024-07-23 21:09:46 +0900

    🎉 add : redis및 rediscache config구현 및 user용 redis dao구현

---
+++ src/main/java/com/sparta/publicclassdev/global/CacheNames.java
@@ -0,0 +1,8 @@
+package com.sparta.publicclassdev.global;
+
+public class CacheNames {
+    public static final String USERBYUSERNAME = "CACHE_USERBYUSERNAME";
+    public static final String ALLUSERS = "CACHE_ALLUSERS";
+    public static final String LOGINUSER = "CACHE_LOGINUSER";
+
+}
