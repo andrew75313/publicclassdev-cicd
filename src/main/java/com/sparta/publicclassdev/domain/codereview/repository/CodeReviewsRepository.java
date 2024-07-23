e0e7cbeed62be483b7b4caa054d891c55d884882
Hyeonjin Kim <andrew75313@gmail.com>
2024-07-23 14:35:03 +0900

    ♻️ refactor: 게시글 조회 시, JPQL 사용으로 변경
    -쿼리문을 사용해서 오류 해결

--- src/main/java/com/sparta/publicclassdev/domain/codereview/repository/CodeReviewsRepository.java
+++ src/main/java/com/sparta/publicclassdev/domain/codereview/repository/CodeReviewsRepository.java
@@ -4,10 +4,12 @@ import com.sparta.publicclassdev.domain.codereview.entity.CodeReviews;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CodeReviewsRepository extends JpaRepository<CodeReviews, Long> {
 
+  @Query("SELECT c FROM CodeReviews c WHERE c.status = 'ACTIVE' ORDER BY c.createdAt DESC")
   Page<CodeReviews> findAllWhereStatusIsActiveOrderByCreatedAtDesc(Pageable pageable);
 }
