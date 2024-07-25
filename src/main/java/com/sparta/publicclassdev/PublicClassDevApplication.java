e75af72ecff378ea7524951aada0a333d08b45b0
Hyeonjin Kim <andrew75313@gmail.com>
2024-07-26 01:57:44 +0900

    ♻️ refactor: Controller Mvc 테스트를 위한 환경 설정
    -JpaConfig 추가하여 EnableJpaAuditing 분리
    -Mvc테스트를 위한 Gradle주입
    -TestCode환경에서 UserDetails의 Principal 설정을 위한 MockSpringSecurityFilter 추기

--- src/main/java/com/sparta/publicclassdev/PublicClassDevApplication.java
+++ src/main/java/com/sparta/publicclassdev/PublicClassDevApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableJpaAuditing
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableCaching
 public class PublicClassDevApplication {
